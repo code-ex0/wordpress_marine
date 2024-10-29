4af7450bc4545310c7c8d957166ed74b785f150b
louis sasse <louis.sasse@ynov.com>
2024-10-29 18:51:00 +0100

    update url

--- app/public/wp-content/cache/wpfc-minified/7zt9ds04/bttct.js
+++ app/public/wp-content/cache/wpfc-minified/7zt9ds04/bttct.js
@@ -1,60 +1,65 @@
-// source --> http://marinenparle.local/wp-content/plugins/reading-progress-bar/public/js/rp-public.js?ver=6.6.1 
-(function( $ ) {
-	'use strict';
-	
-	// The DOM needs to be fully loaded (including graphics, iframes, etc)
-	$(window).on('load', function() {
+// source --> http://192.168.1.47:8000/wp-content/plugins/reading-progress-bar/public/js/rp-public.js?ver=6.6.1
+(function ($) {
+  "use strict";
 
-		// Maximum value for the progressbar
-		var winHeight = $(window).height(),
-		docHeight = $(document).height();
-		var max = docHeight - winHeight;
-		$('.readingProgressbar').attr('max', max);
-		
-		var progressForeground = $('.readingProgressbar').attr('data-foreground');
-		var progressBackground = $('.readingProgressbar').attr('data-background');
-		var progressHeight = $('.readingProgressbar').attr('data-height');
-		var progressPosition = $('.readingProgressbar').attr('data-position');
-		var progressCustomPosition = $('.readingProgressbar').attr('data-custom-position');
-		var progressFixedOrAbsolute = 'fixed';
+  // The DOM needs to be fully loaded (including graphics, iframes, etc)
+  $(window).on("load", function () {
+    // Maximum value for the progressbar
+    var winHeight = $(window).height(),
+      docHeight = $(document).height();
+    var max = docHeight - winHeight;
+    $(".readingProgressbar").attr("max", max);
 
-		// Custom position
-		if (progressPosition == 'custom') {
-			$('.readingProgressbar').appendTo(progressCustomPosition);
-			progressPosition = 'bottom';
-			progressFixedOrAbsolute = 'absolute';
-		}
+    var progressForeground = $(".readingProgressbar").attr("data-foreground");
+    var progressBackground = $(".readingProgressbar").attr("data-background");
+    var progressHeight = $(".readingProgressbar").attr("data-height");
+    var progressPosition = $(".readingProgressbar").attr("data-position");
+    var progressCustomPosition = $(".readingProgressbar").attr(
+      "data-custom-position"
+    );
+    var progressFixedOrAbsolute = "fixed";
 
-		// Styles
-		if ( progressPosition == 'top' ) {
-			var progressTop = '0';
-			var progressBottom = 'auto';
-		} else {
-			var progressTop = 'auto';
-			var progressBottom = '0';
-		}
+    // Custom position
+    if (progressPosition == "custom") {
+      $(".readingProgressbar").appendTo(progressCustomPosition);
+      progressPosition = "bottom";
+      progressFixedOrAbsolute = "absolute";
+    }
 
-		$('.readingProgressbar').css({
-			'background-color' : progressBackground,
-			'color' :  progressForeground,
-			'height' :  progressHeight + 'px',
-			'top' : progressTop,
-			'bottom' : progressBottom,
-			'position' : progressFixedOrAbsolute,
-			'display' : 'block'
-		});
+    // Styles
+    if (progressPosition == "top") {
+      var progressTop = "0";
+      var progressBottom = "auto";
+    } else {
+      var progressTop = "auto";
+      var progressBottom = "0";
+    }
 
-		$('<style>.readingProgressbar::-webkit-progress-bar { background-color: transparent } .readingProgressbar::-webkit-progress-value { background-color: ' + progressForeground + ' } .readingProgressbar::-moz-progress-bar { background-color: ' + progressForeground + ' }</style>')
-		.appendTo('head');
+    $(".readingProgressbar").css({
+      "background-color": progressBackground,
+      color: progressForeground,
+      height: progressHeight + "px",
+      top: progressTop,
+      bottom: progressBottom,
+      position: progressFixedOrAbsolute,
+      display: "block",
+    });
 
-		// Inital value (if the page is loaded within an anchor)
-		var value = $(window).scrollTop();
-		$('.readingProgressbar').attr('value', value);
-		// Maths & live update of progressbar value
-		$(document).on('scroll', function() {
-			value = $(window).scrollTop();
-			$('.readingProgressbar').attr('value', value);
-		});
-	});
-	
-})( jQuery );
+    $(
+      "<style>.readingProgressbar::-webkit-progress-bar { background-color: transparent } .readingProgressbar::-webkit-progress-value { background-color: " +
+        progressForeground +
+        " } .readingProgressbar::-moz-progress-bar { background-color: " +
+        progressForeground +
+        " }</style>"
+    ).appendTo("head");
+
+    // Inital value (if the page is loaded within an anchor)
+    var value = $(window).scrollTop();
+    $(".readingProgressbar").attr("value", value);
+    // Maths & live update of progressbar value
+    $(document).on("scroll", function () {
+      value = $(window).scrollTop();
+      $(".readingProgressbar").attr("value", value);
+    });
+  });
+})(jQuery);
